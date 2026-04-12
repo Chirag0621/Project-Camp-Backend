9762831823864540b27556c963449e52132aaa4b
Chirag Bhadani <kumarchirag978@gmail.com>
2026-04-12 13:08:00 +0530

    Added methods to generate the Access, Refresh and temporary token using jwt in express and crypto module in node js

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,6 +1,7 @@
 import mongoose, { Schema } from 'mongoose'
 import bcrypt from "bcrypt"
-
+import jwt from 'jsonwebtoken';
+import crypto from "crypto";
 
 const userSchema = new Schema(
   {
@@ -83,4 +84,47 @@ userSchema.methods.isPasswordCorrect = async function
   return await bcrypt.compare(password, this.password) // returns true if the password is correct 
   // Password checking is done using hashing as if you are giving the correct password the hashing is same to the saved one...
 }
+userSchema.methods.generateAccessToken = function(){
+  return jwt.sign(
+
+    // These first information is called payload 
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {expiresIn: process.env.ACCESS_TOKEN_EXPIRY}
+  )
+}
+userSchema.methods.generateRefreshToken = function(){
+
+  //Payload
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  )
+}
+
+userSchema.methods.generateTemporaryToken = function(){
+  //Generating long random string 
+  const unhashedToken = crypto.randomBytes(20).toString("hex")
+
+  // Encypting the string
+  const hashedToken = crypto
+    .createHash("sha256")
+    .update(unhashedToken)
+    .digest("hex")
+  
+  
+  const tokenExpiry = Date.now + (20*60*1000) // Adding 20 minutes
+  //sending it back
+  return {unhashedToken, hashedToken, tokenExpiry}
+}
+
+
 export const user = mongoose.model('User', userSchema);
