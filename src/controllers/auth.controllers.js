a8a7769874faf2a0059c0ceb114d2bc73cdc7f13
Chirag Bhadani <kumarchirag978@gmail.com>
2026-04-17 00:02:58 +0530

    Added Reset_Password_Controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -313,6 +313,41 @@ const forgotPasswordRequest = asyncHandler(async(req, res) => {
       )
     )
 })
+
+const resetForgotPassword = asyncHandler(async(req, res) => {
+  const {resetToken} = req.params
+  const {newPassword} = req.body
+
+  let hashedToken = crypto
+      .createHash('sha256')
+      .update(resetToken)
+      .digest("hex")
+
+      const user = await User.findOne({
+        forgotPasswordToken: hashedToken,
+        forgotPasswordExpiry: {$gt: Date.now()}
+      })
+
+      if(!user){
+        throw new ApiError(489, "Token is invalid or expired")
+      }
+
+      user.forgotPasswordExpiry = undefined;
+      user.forgotPasswordExpiry = undefined;
+
+      user.password = newPassword
+      await user.save({validateBeforeSave: false})
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        {},
+        "Password reset successfully"
+      )
+    )
+});
 export { 
   registerUser,
   login,
