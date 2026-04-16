6fce7c98a5e2a9a50842a74313d0663179eb7e18
Chirag Bhadani <kumarchirag978@gmail.com>
2026-04-16 22:36:57 +0530

    Added Verify-Email in controller

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -122,43 +122,72 @@ const login = asyncHandler(async (req, res) => {
     );
 });
 
-const logoutUser = asyncHandler( async(req, res) => {
+const logoutUser = asyncHandler(async (req, res) => {
   await User.findByIdAndUpdate(
     req.user._id,
     {
-      $set : {
-        accessToken: ""
-      }
+      $set: {
+        accessToken: '',
+      },
     },
     {
-      // new will give the most updated or newer object they have 
+      // new will give the most updated or newer object they have
       new: true,
-    },
+    }
   );
   const options = {
     httpOnly: true,
     secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie('accessToken', options)
+    .clearCookie('refreshToken', options)
+    .json(new ApiResponse(200, {}, 'User logged out'));
+});
+
+const getCurrentUser = asyncHandler(async (req, res) => {
+  return res.status(200).json(new ApiResponse(200, req.user, 'Current user fetched successfully'));
+});
+
+const verifyEmail = asyncHandler(async (req, res) => {
+  // Just like req.body req.params give access to the url itself
+  const { verificationToken } = req.params;
+
+  if (!verificationToken) {
+    throw new ApiError(400, "Email verification token is missing");
   }
-  return res
-      .status(200)
-      .clearCookie("accessToken", options)
-      .clearCookie("refreshToken", options)
-      .json(
-        new ApiResponse(200, {}, "User logged out")
-      )
 
+  //once we have the token we have to hash it again
+  let hashedToken = crypto.createHash('sha256').update('verificationToken').digest('hex');
+
+  const user = await User.findOne({
+    emailVerificationToken: hashedToken,
+    emailVerificationExpiry: { $gt: Date.now() },
+  });
+  if (!user) {
+    throw new ApiError(400, "Token is expired or Invalid");
+  }
+
+  user.emailVerificationToken = undefined;
+  user.emailVerificationExpiry = undefined;
+
+  user.isEmailVerified = true
+  await user.save({validateBeforeSave: false})
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          isEmailVerified = true
+        },
+        "Email is verified"
+
+      )
+    )
 
 });
 
-const getCurrentUser = asyncHandler(async(req, res) => {
-  return res
-      .status(200)
-      .json(
-        new ApiResponse(
-          200,
-          req.user,
-          "Current user fetched successfully"
-        )
-      )
-});
-export { registerUser, login, logoutUser, getCurrentUser };
+export { registerUser, login, logoutUser, getCurrentUser, verifyEmail };
