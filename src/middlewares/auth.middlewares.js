85aafc56a2b24157ae45b966a7a5ba1abb47e238
Chirag Bhadani <kumarchirag978@gmail.com>
2026-04-30 03:16:59 +0530

    Added middleware for validate project permission

--- src/middlewares/auth.middlewares.js
+++ src/middlewares/auth.middlewares.js
@@ -1,29 +1,59 @@
-import jwt from "jsonwebtoken";
-import { User } from "../models/user.models.js"
-import { ApiError} from "../utils/api-error.js"
-import { asyncHandler } from "../utils/async-handler.js"
+import jwt from 'jsonwebtoken';
+import { User } from '../models/user.models.js';
+import { ProjectMember } from '../models/projectmember.models.js';
+import { ApiError } from '../utils/api-error.js';
+import { asyncHandler } from '../utils/async-handler.js';
+import mongoose from 'mongoose';
 
-export const verifyJWT = asyncHandler( async(req, res, next) => 
-  {
-    // ? is for optional we are able to access the access token we can access it
-    const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer", "")
+export const verifyJWT = asyncHandler(async (req, res, next) => {
+  // ? is for optional we are able to access the access token we can access it
+  const token = req.cookies?.accessToken || req.header('Authorization')?.replace('Bearer', '');
 
-    if(!token){
-      throw new ApiError(401, "Unauthorized request")
-    }
-
-    try{
-      const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET)
-      const user = await User.findById(decodedToken?._id).select("-password -refreshToken -emailVerificationToken -emailVerificationExpiry");
-
-      if(!user){
-        throw new ApiError(401, "Invalid access token")
-      }
-      req.user = user
-      next()
-    }
-    catch{
-      throw new ApiError(401, "Invalid access token")
-    }
+  if (!token) {
+    throw new ApiError(401, 'Unauthorized request');
   }
-)
+
+  try {
+    const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+    const user = await User.findById(decodedToken?._id).select(
+      '-password -refreshToken -emailVerificationToken -emailVerificationExpiry'
+    );
+
+    if (!user) {
+      throw new ApiError(401, 'Invalid access token');
+    }
+    req.user = user;
+    next();
+  } catch {
+    throw new ApiError(401, 'Invalid access token');
+  }
+});
+
+export const validateProjectPermission = (roles = []) => {
+  asyncHandler(async (req, res, next) => {
+    const { projectId } = req.params;
+
+    if (!projectId) {
+      throw new ApiError(400, 'Project is missing');
+    }
+
+    const project = await ProjectMember.findOne({
+      project: new mongoose.Types.ObjectId(projectId),
+      user: new mongoose.Types.ObjectId(req.user._id),
+    });
+
+    if (!project) {
+      throw new ApiError(400, 'Project not found');
+    }
+
+    const givenRole = project?.role;
+
+    req.user.role = givenRole;
+
+    if (!roles.includes(givenRole)) {
+      throw new ApiError(403, 'You do not have permission to perform this action');
+    }
+
+    next();
+  });
+};
